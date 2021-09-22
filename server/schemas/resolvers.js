19ce3bbcb32f33369ba88ff4a1a9363b78211ee2
Burak Aksu <sabburak26@gmail.com>
2021-09-21 20:44:11 -0400

    added a resolver to savebook and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,48 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('savedBooks');
-    },
     user: async (parent, { username }) => {
-      return User.findOne({userName}).populate('savedBooks')
+      return User.findOne({userName})
     },
-    books
+    books: async (parent, { username }) => {
+      const user = User.findOne({ username }).populate('savedBooks')
+      const { savedBooks } = user;
+      return savedBooks
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, arg) => {
+      const user  = await User.create(arg);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError ("Incorrect email and password");
+      }
+
+      const correctPw = user.isCorrectPassword(password)
+
+      if(!correctPw) {
+        throw new AuthenticationError('Incorrect email or password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, {user, body} ) => {
+      const book = await Book.create({ user, book });
+
+      await User.findOneAndUpdate(
+        { username: user },
+        { $addToSet: { savedBooks: body }}
+      );
+
+      return book;
+    }
   }
 }
 
